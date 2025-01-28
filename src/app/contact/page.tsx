965709ba14e63a52099bdc3a433a15283277274d
Danyal072 <Danyal07@proton.me>
2025-01-28 14:40:36 +0500

    Contact Page Responsive for Phone

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,19 +9,19 @@ import Form_Section from '../components/contact_sections/Form_Section'
 export default function page() {
   return (
     <>
-        <div id="main_area">
+        <div id="main_area" className='w-full h-full'>
             <Header />
             <div id="contact-page-elements">
                 <div id="header_Hero">
                     <Header_Contact />
                 </div>
-                <div id="form-area" className='flex flex-col lg:flex-row gap-4'>
-                    <div id="side-bar" className='w-1/4 h-full hidden lg:block'>
+                <div id="form-area" className='flex flex-row gap-4 justify-center items-center h-[60vh]'>
+                    <div id="side-bar" className='w-1/4 h-full hidden xl:block'>
                         <SideBar />
                     </div>
-                    <div id="f_section" className='relative top-[8rem] w-[75%] h-full flex justify-center items-center'>
+                    <div id="f_section" className='relative top-[8rem] w-full xl:w-[75%] h-full flex justify-center items-center'>
                         
-                        <div id="inner_section" className='w-1/2 h-full'>
+                        <div id="inner_section" className='w-11/12 lg:w-10/12 xl:w-1/2 h-full'>
                             <Form_Section />
                         </div>
                     </div>
